5e096e551203f724efb97b1f2f0df8326b28cef7
Ankur Goswami <ankur@relianceco.com>
2016-01-30 00:10:36 -0800

    -changed textParser's function to exports.

--- textParser.js
+++ textParser.js
@@ -1,6 +1,6 @@
 var request = "eggs, milk, pasta";
 
-function textParser(var request)  {
+exports.textParser = function(var request)  {
 	if (request.indexOf(',') > -1) {
 	    var noCommas = request.split(',');
 	    console.log(noCommas);
