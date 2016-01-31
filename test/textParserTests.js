22f93d882af39c43996b235d38c1ddfb261c46ec
Ankur Goswami <agoswam3@ucsc.edu>
2016-01-31 00:21:42 -0800

    editt textParserTest

--- test/textParserTests.js
+++ test/textParserTests.js
@@ -6,10 +6,10 @@ var textParser = require('../textParser');
 describe('textParser test', function(){
     describe('validInput', function(){
         it("Should parse out the materials", function(){
-            var items = "Eggs, Milk, Pasta: 3907 Rincon Ridge Dr., Santa Rosa, California";
-            textParser.textParser(items, function(resultItems, resultAddress){
-                var expectedItems = ['Eggs', 'Milk', 'Pasta'];
-                var expectedAddress = '3907 Rincon Ridge Dr., Santa Rosa, California';
+            var items = "Eggs, Milk: 7546 Stevenson service road, Santa Rosa, California";
+            textParser.parseText(items, function(resultItems, resultAddress){
+                var expectedItems = ['Eggs', 'Milk'];
+                var expectedAddress = '7546 Stevenson service road, Santa Rosa, California';
                 resultItems.should.deep.equal(expectedItems);
                 resultAddress.should.equal(expectedAddress);
             });
