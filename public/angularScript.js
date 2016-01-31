f84a23613a1028f73ec38d7e5d1c1421bccb4977
Justin Lee <jus.lee18@gmail.com>
2016-01-31 03:53:23 -0800

    icon close

--- public/angularScript.js
+++ public/angularScript.js
@@ -9,10 +9,10 @@ myApp.controller('mainController', ['$scope', '$filter', '$http',  function ($sc
     $scope.loadedItems;
     $scope.testing = {};
 
-    var listOfItems = [];
+    $scope.listOfItems = [];
     $scope.cartItemObjects = [];
     $scope.cartPrice = 0;
-
+    $scope.filtering ;
     $scope.categories = ['Food', 'Health', 'Clothing', 'Education' ];
 
     $scope.innerList = function(index){
@@ -21,24 +21,42 @@ myApp.controller('mainController', ['$scope', '$filter', '$http',  function ($sc
         console.log($scope.filtering);
         $scope.objects = $filter('filter')($scope.items[0], "Food");
         console.log($scope.objects);
-    }
+    };
 
-    $scope.addItem = function(index, arr, product){
+    $scope.addItem = function(index, product){
         //console.log(index);
         //console.log(arr[index]);
 
         //item name list
 
-        $scope.listOfItems = listOfItems.push(product.name);
-        console.log(listOfItems);
+        $scope.listOfItems.push(product.name);
+        console.log($scope.listOfItems);
 
         //item object list
         $scope.cartItemObjects.push(product);
-
         //console.log(cartItemObjects);
+
+        //CART PRICE CHANGE
         var productPrice = parseFloat(product.price, 10);
         $scope.cartPrice += productPrice;
-    }
+    };
+
+    $scope.close = function(index, product) {
+        console.log("close " + index);
+        //$scope.listOfItems = listOfItems.splice(index, 1);
+        //console.log(listOfItems);  
+
+        // //CART PRICE CHANGE
+        var productPriceMinus = parseFloat(product.price, 10);
+        $scope.cartPrice -= productPriceMinus;  
+        console.log(productPriceMinus);     
+
+        //var index1 = $scope.listOfItems.indexOf(product);
+        $scope.listOfItems.splice(index, 1);
+        $scope.cartItemObjects.splice(index, 1);
+
+        console.log($scope.listOfItems);              
+    };
 
     $scope.saveCustomer = function(status, response) {
         $http.post('https://glacial-inlet-76332.herokuapp.com/api/v1/payment', { token: response.id, items: listOfItems});
